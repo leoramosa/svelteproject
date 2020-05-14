e4a8a88ba4ae7a4d3927dc03153485ebdc654677
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-05-14 18:40:14 -0500

    estilando el componente header y sidebar, tambien creamos los siguiente componentes profile, stories y footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+  let year = newDate().getFullYear();
+  let footerText = `© ${year} Pugstagram From Platzi`;
+</script>
+
+<div class="Footer">
+  <div class="Footer-content">
+    <div class="Footer-copy">
+      <p>
+        <span>{footerText}</span>
+      </p>
+    </div>
+  </div>
+</div>
