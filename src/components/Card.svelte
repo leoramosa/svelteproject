c4daa1b1add5c3bf2c8c4385a16b4cf26c35b2af
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-05-14 20:35:20 -0500

    se termino de agregar las propiedades del api a los componentes

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -126,10 +126,10 @@
   <div class="Card-container">
     <div class="Card-header">
       <div class="Card-user">
-        <img src="https://arepa.s3.amazonaws.com/elmo002.jpg" alt="" />
+        <img src={avatar} alt={username} />
         <h2>
-          Elmo.pug
-          <span>Lima, Perú</span>
+          {username}
+          <span>{location}</span>
         </h2>
       </div>
       <div class="Card-settings">
@@ -138,7 +138,7 @@
     </div>
     <div class="Card-photo">
       <figure>
-        <img src="https://arepa.s3.amazonaws.com/elmo002.jpg" alt="" />
+        <img src={photo} alt={username} />
       </figure>
     </div>
     <div class="Card-icons">
@@ -151,9 +151,9 @@
       </div>
     </div>
     <div class="Card-description">
-      <h3>leo.ramos</h3>
-      <span>Hola!</span>
+      <h3>{username}</h3>
+      <span>{postComment}</span>
     </div>
-    <Comments />
+    <Comments {comments} />
   </div>
 </div>
