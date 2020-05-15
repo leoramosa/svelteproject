fc2defe281bd3bd53febacc7733291725a5bf1a9
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-05-14 20:31:42 -0500

    se esta agregando datos desde una api

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,6 +3,15 @@
   let footerText = `© ${year} Pugstagram From Platzi`;
 </script>
 
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
 <div class="Footer">
   <div class="Footer-container">
     <div class="Footer-copy">
